42dde57cb4dd6aa43390ba517f9b1454d6964d1a
TTXXSS <1374230455@qq.com>
2019-07-25 17:49:46 +0800

    add Table.vue and Button.vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vue from 'vue'
+import Antd from 'ant-design-vue'
+import App from './App'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Antd)
+/* eslint-disable no-new */
 
 Vue.config.productionTip = false
 
